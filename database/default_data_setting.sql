b115f0d9058eae806df669a038c206fe48c3e426
IOUKI <quw112233456@gmail.com>
2023-04-29 16:06:34 +0800

    update

--- database/default_data_setting.sql
+++ database/default_data_setting.sql
@@ -1,3 +1,3 @@
 -- admin account setup
 INSERT INTO admin_user(id, title, account, hash_key, hash_salt, created_at)
-VALUES('R001', 'admin', '5fd2b03b712ff1b7e53b5bb896e1acce5c9e10bc220bb142774eaf183f551664', 'E]0-/yO4t2', now())
+VALUES('R001', 'admin', 'lan0225', '5fd2b03b712ff1b7e53b5bb896e1acce5c9e10bc220bb142774eaf183f551664', 'E]0-/yO4t2', now())
